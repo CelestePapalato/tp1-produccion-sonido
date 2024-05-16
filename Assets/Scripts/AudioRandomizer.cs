c03312d66dbcdb57481a0428962f5b983b261c4a
CelestePapalato <50720541+CelestePapalato@users.noreply.github.com>
2024-05-16 15:31:34 -0300

    AudioRandomizer ahora reproduce el clip tras un tiempo de espera randomizado.  Añadidos escena de ejemplo y audio para el testeo

--- Assets/Scripts/AudioRandomizer.cs
+++ Assets/Scripts/AudioRandomizer.cs
@@ -1,26 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class AudioRandomizer : MonoBehaviour
 {
+    [SerializeField] bool playOnStart = true;
+    [SerializeField] float minTiempoEspera;
+    [SerializeField] float maxTiempoEspera;
     [SerializeField] AudioClip[] audioClips;
-    [SerializeField] string[] strings;
+
     AudioSource audioSource;
+
+    bool isActive = true;
+    public bool IsPlaying
+    {
+        get { return isActive; }
+        set
+        {
+            if (value && value != isActive)
+            {
+                isActive = value;
+                StartCoroutine(playNextClip());
+            }
+            if (!value && value != isActive)
+            {
+                isActive = value;
+                StopCoroutine(playNextClip());
+            }
+        }
+    }
 
     private void Awake()
     {
+        Mathf.Max(0f, minTiempoEspera);
+        Mathf.Max(0f, minTiempoEspera, maxTiempoEspera);
         audioSource = GetComponent<AudioSource>();
     }
 
-    private void Update()
+    private void Start()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if(playOnStart) { StartCoroutine(playNextClip());}
+        else { isActive = false; }
+    }
+
+    IEnumerator playNextClip()
+    {
+        while (isActive)
         {
-            AudioClip clip = (AudioClip) ObjectRandomizer.GetRandom(audioClips);
-            if(audioSource) { audioSource.clip = clip; }
-            string text = (string) ObjectRandomizer.GetRandom(strings);
-            Debug.Log(text);
+            float time = Random.Range(minTiempoEspera, maxTiempoEspera);
+            Debug.Log(name + " reproduciendo siguiente clip en " + time + "s.");
+            yield return new WaitForSeconds(time);
+            AudioClip clip = (AudioClip)ObjectRandomizer.GetRandom(audioClips);
+            if (audioSource) { audioSource.clip = clip; }
+            audioSource.Play();
+            Debug.Log(name + " reproduciendo clip: " + clip.name + ".");
+            yield return new WaitForSeconds(audioClips.Length);
         }
     }
 }
